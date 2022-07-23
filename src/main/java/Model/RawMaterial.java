7fcdbbc2c6b36a63c9ce34d02d215b65b0000f53
HappyMan17 <sebasgrisales16@gmail.com>
2022-07-23 13:45:04 -0500

    Se añade el paquete model con las clases Supplier y RawMaterial

---
+++ src/main/java/Model/RawMaterial.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author happy
+ */
+public class RawMaterial {
+    //Attribute
+    private String name;
+    private int unitPrice, code, supplierCode, amount;
+    
+    //Methods
+    public RawMaterial(String name, int unitPrice, int code, int supplierCode, 
+        int amount){
+        this.name = name;
+        this.unitPrice = unitPrice;
+        this.code = code;
+        this.supplierCode = supplierCode;
+        this.amount = amount;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getSupplierCode() {
+        return supplierCode;
+    }
+
+    public void setSupplierCode(int supplierCode) {
+        this.supplierCode = supplierCode;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+}
