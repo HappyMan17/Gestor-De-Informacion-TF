6cb3ac59dae827e079e4888cf150c93f326c9610
HappyMan17 <sebasgrisales16@gmail.com>
2022-07-23 15:43:48 -0500

    Se añaden las clases de company, product y production

---
+++ src/main/java/Model/Company.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author happy
+ */
+public class Company {
+    //Atribute
+    private ArrayList<Supplier> suppliers;
+    private final ArrayList<RawMaterial> rawMaterials = new ArrayList<>();
+    private Production production;
+    private ArrayList<Product> products = new ArrayList<>();
+    
+    //Methods
+    public Company(ArrayList<Supplier> suppliers){
+        this.suppliers = suppliers;
+        this.production = new Production();
+    }
+    
+    public ArrayList<Supplier> getSuppliers() {
+        return suppliers;
+    }
+
+    public void addSupplier(Supplier supplier) {
+        this.suppliers.add(supplier);
+    }
+
+    public ArrayList<RawMaterial> getRawMaterials() {
+        return rawMaterials;
+    }
+
+    public void addRawMaterials(RawMaterial rawMaterial) {
+        this.rawMaterials.add(rawMaterial);
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+    
+    //public void processRawMaterial(){}
+    
+}
